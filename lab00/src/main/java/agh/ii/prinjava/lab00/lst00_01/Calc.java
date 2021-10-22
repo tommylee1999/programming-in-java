06d20689c85987275dbd7579029bdc8cbe06af8e
tommylee1999 <73222982+tommylee1999@users.noreply.github.com>
2021-10-22 03:57:49 +0200

    Lab0

--- lab00/src/main/java/agh/ii/prinjava/lab00/lst00_01/Calc.java
+++
@@ -1,7 +0,0 @@
-package agh.ii.prinjava.lab00.lst00_01;
-
-public class Calc {
-    public static int add (int a, int b) {
-        return a + b;
-    }
-}
--- lab00/src/main/java/agh/ii/prinjava/lab00/lst00_01/Main.java
+++
@@ -1,7 +0,0 @@
-package agh.ii.prinjava.lab00.lst00_01;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("add(1,2) = " + Calc.add(1,2));
-    }
-}
